4c4cabb666853d035a97afa902484ccda04ecec3
Abid-Hasan <ahsnigdho@gmail.com>
2021-11-23 11:00:54 +0600

    version 1.0

---
+++ src/components/OrderReview/OrderReview.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/useProducts';
+import { clearTheCart, deleteFromDb } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import { useNavigate } from 'react-router';
+
+const OrderReview = () => {
+
+
+    const [products] = useProducts();
+    const [cart, setCart] = useCart(products);
+    const navigate = useNavigate();
+
+    const handlePlaceOrder = () => {
+        navigate('/placeOrder');
+        setCart([]);
+        clearTheCart();
+    }
+
+    const handleRemove = key => {
+        const newCart = cart.filter(product => product.key !== key);
+        setCart(newCart);
+        deleteFromDb(key);
+    }
+
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.key}
+                        product={product}
+                        handleRemove={handleRemove}
+                    />)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    <button onClick={handlePlaceOrder} className="btn-regular">Place Order</button>
+                </Cart>
+            </div>
+        </div>
+    );
+};
+
+export default OrderReview;
